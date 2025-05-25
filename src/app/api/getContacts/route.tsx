41e110def5df9b3061bf995c33bcaa93cb20916d
sencoders <shreejachaudhary14@gmail.com>
2025-05-25 20:39:38 +0530

    partial

--- src/app/api/getContacts/route.tsx
+++ src/app/api/getContacts/route.tsx
@@ -57,7 +57,7 @@ import { NextResponse } from 'next/server'
 import { createClient } from '@/app/utils/supabase/server'
 
 export async function GET() {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   const {
     data: { user },
